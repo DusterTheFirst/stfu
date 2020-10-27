4d418f3937b9b2ff992b38c9c9a8e7a0979e173d
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2020-10-27 09:56:41 -0400

    fix build script for CI

--- backend/build.rs
+++ backend/build.rs
@@ -1,20 +1,19 @@
 use regex::Regex;
-use std::{
-    fs::File,
-    io::{Read, Write},
-};
+use std::{env, fs};
 
 fn main() -> anyhow::Result<()> {
-    println!("cargo:rerun-if-changed=.env");
+    if fs::metadata(".env").is_ok() && env::var("CI").map(|v| v == "true").unwrap_or_default() {
+        println!("cargo:rerun-if-changed=.env");
 
-    let mut dotenv_contents = String::new();
-    File::open(".env")?.read_to_string(&mut dotenv_contents)?;
+        let contents = fs::read_to_string(".env")?;
 
-    File::create("example.env")?.write_all(
-        Regex::new(r#"".*" # Private"#)?
-            .replace_all(&dotenv_contents, "")
-            .as_bytes(),
-    )?;
+        fs::write(
+            "example.env",
+            Regex::new(r#"".*" # Private"#)?
+                .replace_all(&contents, "")
+                .as_bytes(),
+        )?
+    }
 
     Ok(())
 }
